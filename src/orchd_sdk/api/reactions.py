1125a0f9183e9e9e1671c8e555b5b2b5c8e9b4c8
Mathias Santos de Brito <mathias.santos.de.brito@tu-berlin.de>
2024-01-24 18:31:29 +0100

    finish pydantic 2 migration

--- src/orchd_sdk/api/reactions.py
+++ src/orchd_sdk/api/reactions.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import parse_obj_as
+from pydantic import TypeAdapter
 
 from orchd_sdk.models import ReactionInfo, ReactionTemplate, Ref
 
@@ -12,14 +12,15 @@ class ReactionClient:
 
     async def get_reactions(self) -> List[ReactionInfo]:
         reactions = await self.orchd_client.get('/reactions')
-        return parse_obj_as(List[ReactionInfo], reactions)
+        adapter = TypeAdapter(List[ReactionInfo])
+        return adapter.validate_python(reactions)
 
     async def get_reaction(self, reaction_id: str) -> ReactionInfo:
         reaction = await self.orchd_client.get(f'/reactions/{reaction_id}')
         return ReactionInfo(**reaction)
 
     async def add_reaction(self, template_id: str) -> ReactionInfo:
-        response = await self.orchd_client.post('/reactions', Ref(id=template_id).model_dump_json())
+        response = await self.orchd_client.post('/reactions', Ref(id=template_id).model_dump())
         return ReactionInfo(**response)
 
     async def remove_reaction(self, reaction_id: str) -> str:
@@ -28,14 +29,15 @@ class ReactionClient:
 
     async def get_reaction_templates(self) -> List[ReactionTemplate]:
         templates = await self.orchd_client.get('/reactions/templates/')
-        return parse_obj_as(List[ReactionTemplate], templates)
+        adapter = TypeAdapter(List[ReactionTemplate])
+        return adapter.validate_python(templates)
 
     async def get_reaction_template(self, template_id: str) -> ReactionTemplate:
         template = await self.orchd_client.get(f'/reactions/templates/{template_id}')
         return ReactionTemplate(**template)
 
     async def add_reaction_template(self, template: ReactionTemplate) -> ReactionTemplate:
-        response = await self.orchd_client.post('/reactions/templates/', template.dict())
+        response = await self.orchd_client.post('/reactions/templates/', template.model_dump())
         return ReactionTemplate(**response)
 
     async def remove_reaction_template(self, template_id: str) -> str:
@@ -44,7 +46,7 @@ class ReactionClient:
 
     async def add_sink_to_reaction(self, reaction_id: str, template_id: str) -> str:
         response = await self.orchd_client.post(f'/reactions/{reaction_id}/sinks',
-                                                Ref(id=template_id).dict())
+                                                Ref(id=template_id).model_dump())
         return response
 
     async def remove_sink_from_reaction(self, reaction_id: str, sink_id: str) -> str:
